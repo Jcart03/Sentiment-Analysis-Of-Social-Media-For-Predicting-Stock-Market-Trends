8af8dd98c0166567a74433e7248698b830ffd28e
Jcart03 <122915076+Jcart03@users.noreply.github.com>
2025-04-22 10:44:07 +0100

    Update cleaner.py

--- Dissertation/Code/training/tools/cleaner.py
+++ Dissertation/Code/training/tools/cleaner.py
@@ -24,10 +24,3 @@ def map(raw_path, delimiter, label, text,  map, clean_path):
     print(ds.head())
     
     return ds
-    
-    
-    
-    
-map("Dissertation/datasets/berTweet/raw/financial_tweets.csv", ',', 'sentiment','description', map={'Bearish':0, 'Neutral':1, 'Bullish':2}, clean_path='Dissertation/datasets/berTweet/clean')
-map("Dissertation/datasets/berTweet/raw/tweets_labelled_09042020_16072020.csv", ';', 'sentiment', 'text', map={'negative':0, 'neutral':1, 'positive':2}, clean_path='Dissertation/datasets/berTweet/clean')
-map("Dissertation/datasets/berTweet/raw/Twitter_Data.csv", ',', 'category', 'clean_text', map={-1:0, 0:1, 1:2}, clean_path='Dissertation/datasets/berTweet/clean')
